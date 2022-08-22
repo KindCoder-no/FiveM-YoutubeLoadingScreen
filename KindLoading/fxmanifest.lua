1269fb97096b8aac434849eb1b2cbd5bf3a64f64
KindCoder <40148297+KindCoder-no@users.noreply.github.com>
2022-08-22 20:53:21 +0200

    Create fxmanifest.lua

---
+++ KindLoading/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'KindCoder-no'
+description 'Discord : https://discord.gg/yxTvKgTQHe'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+
+files {
+    'index.html',
+    'css/style.css',
+}
